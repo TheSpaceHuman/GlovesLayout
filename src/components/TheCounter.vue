378eaf764e54c2c6c281e8b2535076fa0822b20c
TheSpaceHuman <kolodyazhnyartem@gmail.com>
2019-10-03 13:59:00 +0300

    full done

---
+++ src/components/TheCounter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="counter-wrapper">
+    <el-input-number
+      :max="max"
+      :min="min"
+      :name="name"
+      v-model="value"></el-input-number>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {
+        default: 1
+      },
+      min: {
+        default: 1
+      },
+      max: {
+        default: 10
+      },
+      name: {
+        default: 'product-counter'
+      }
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../scss/utils/vars";
+  .el-input-number__decrease:hover:not(.is-disabled) ~ .el-input .el-input__inner:not(.is-disabled), .el-input-number__increase:hover:not(.is-disabled) ~ .el-input .el-input__inner:not(.is-disabled) {
+    border-color: $primary-color;
+  }
+  .el-input-number__decrease:hover, .el-input-number__increase:hover {
+    color: $primary-color;
+  }
+  .counter-wrapper {}
+
+</style>
---
+++ src/components/TheInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="input-wrapper">
+    <el-input v-if="phone" v-mask="'+7 (XXX) XXX-XX-XX'" v-model="value" :name="name" :type="type" :placeholder="placeholder"></el-input>
+    <el-input v-else v-model="value" :name="name" :type="type" :placeholder="placeholder"></el-input>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {
+        default: ''
+      },
+      name: {
+        default: 'input-name'
+      },
+      type: {
+        default: 'text'
+      },
+      placeholder: {
+        default: ''
+      },
+      phone: {
+        default: false
+      }
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../scss/utils/vars";
+  .el-input.is-active .el-input__inner, .el-input__inner:focus {
+    border-color: $primary-color;
+  }
+  .el-textarea.is-active .el-textarea__inner, .el-textarea__inner:focus {
+    border-color: $primary-color;
+  }
+  .input-wrapper {}
+
+</style>
